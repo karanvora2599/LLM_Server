1872cf60e757f2dc1f0063f791175546e1701104
Karan Vora <kv2154@nyu.edu>
2024-10-10 17:14:45 -0400

    Added config file and test for efficient deployement.

---
+++ gunicorn.conf.py
@@ -0,0 +1,10 @@
+# gunicorn.conf.py
+
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "uvicorn.workers.UvicornWorker"
+bind = "0.0.0.0:8000"
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
